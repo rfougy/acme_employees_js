fdabacc1e444822391ec5cb933c81a0c8ed6f8f8
Riviere Fougy <rivierefougy@gmail.com>
2021-07-13 15:49:24 -0400

    created findManagerFor and findCoworkersFor functions

--- main.js
+++ main.js
@@ -13,4 +13,24 @@ function findEmployeeByName(name, employeeArr) {
       else continue;
     }
   }
+  return "Name could not be found in database.";
+}
+
+function findManagerFor(employeeObj, employeeArr){
+  for (let i = 0; i < employeeArr.length; i++) {
+    let managerObj = employeeArr[i];
+    if (managerObj.id === employeeObj.managerId) return managerObj;
+    else continue;
+  }
+  return "Manager of ${employeeObj.name} could not be found in database.";
+}
+
+function findCoworkersFor(employeeObj, employeeArr){
+  let coworkersArr = [];
+  for (let i = 0; i < employeeArr.length; i++) {
+    let coworkerObj = employeeArr[i];
+    if (coworkerObj.managerId = employeeObj.managerId) coworkersArr.push(coworkerObj);
+    else continue;
+  }
+  return (coworkersArr.length) ? coworkersArr : "Coworkers of ${employeeObj.name} could not be found in database.";
 }
